70b8fc299aca94d514112e6a808c8c89b9af1ed7
Ary Lins <agl@cesar.org.br>
2019-02-12 14:39:32 -0300

    feat: cria gets e sets para Estabelecimento

--- src/main/java/com/codenation/mapfood/model/Estabelecimento.java
+++ src/main/java/com/codenation/mapfood/model/Estabelecimento.java
@@ -11,4 +11,51 @@ public class Estabelecimento {
     private BigDecimal latitude;
     private String tipoPratos;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getTipoPratos() {
+        return tipoPratos;
+    }
+
+    public void setTipoPratos(String tipoPratos) {
+        this.tipoPratos = tipoPratos;
+    }
 }
