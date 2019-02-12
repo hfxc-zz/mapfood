15d4e5b2b982d696ab0684352bf11feeb97319f3
Ary Lins <agl@cesar.org.br>
2019-02-12 14:32:02 -0300

    feat: cria os models das entidades

---
+++ src/main/java/com/codenation/mapfood/model/Estabelecimento.java
@@ -0,0 +1,14 @@
+package com.codenation.mapfood.model;
+
+import java.math.BigDecimal;
+
+public class Estabelecimento {
+
+    private int id;
+    private String nome;
+    private String municipio;
+    private BigDecimal longitude;
+    private BigDecimal latitude;
+    private String tipoPratos;
+
+}
