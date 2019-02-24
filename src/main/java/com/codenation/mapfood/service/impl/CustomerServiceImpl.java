10fda7ded907a4caa95386eddb0c22aa5e27b66a
Heitor Fontes <hfxc@cin.ufpe.br>
2019-02-24 14:02:35 -0300

    Minor adjustments, moving classes packages and access modifiers

--- src/main/java/com/codenation/mapfood/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codenation/mapfood/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,8 @@
-package com.codenation.mapfood.service;
+package com.codenation.mapfood.service.impl;
 
 import com.codenation.mapfood.model.Customer;
 import com.codenation.mapfood.repository.CustomerRepository;
+import com.codenation.mapfood.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
