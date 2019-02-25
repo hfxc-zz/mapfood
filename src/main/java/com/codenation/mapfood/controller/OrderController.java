3ab804a94a22ba68ab446cf9dfe484761557201c
Ary Lins <agl@cesar.org.br>
2019-02-25 14:17:52 -0300

    LocalDate deveria ser LocalDateTime
    para creation date e finish date de Orders. Também corrigindo bug de unidades

--- src/main/java/com/codenation/mapfood/controller/OrderController.java
+++ src/main/java/com/codenation/mapfood/controller/OrderController.java
@@ -26,7 +26,7 @@ public class OrderController {
         });
 
         OrderDTO response = new OrderDTO(storedOrder.getId(), storedOrder.getCustomer().getId(),
-                productIdList, storedOrder.getRestaurant().getName(), storedOrder.getCreationDate(),
+                productIdList, storedOrder.getRestaurant().getName(), storedOrder.getCreationDate().toLocalDate(),
                 storedOrder.getDelivery().getId(), storedOrder.getStatus());
 
         return response;
